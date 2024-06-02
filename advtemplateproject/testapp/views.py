fda54700e0dba657f3bf302f875c4277f34e930d
gowtham <gowthamjanga7@gmail.com>
2024-06-02 18:30:11 +0530

    first commit by Gowtham

---
+++ advtemplateproject/testapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home_view(request):
+    return render(request, 'testapp/home.html')
+
+
+def movie_view(request):
+    return render(request, 'testapp/movie.html')
+
+
+def politics_view(request):
+    return render(request, 'testapp/politics.html')
+
+
+def sports_view(request):
+    return render(request, 'testapp/sports.html')
